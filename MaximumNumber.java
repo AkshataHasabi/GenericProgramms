96112592645650d79f07f7098d5392e95af0341a
AkshataHasabi <akshuh818@gmail.com>
2022-02-20 09:20:23 +0530

    ADDED:finding maximum string in a given set of strings using generics

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -5,14 +5,30 @@ import java.util.Scanner;
 public class MaximumNumber<E extends Comparable<E>> {
 	public static int MAX;
 	public static float MAX1;
+	public static String MAX2;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java Generic Programms");
+		System.out.println("Enter a option:"+"\n"+"1.Integer Number"+"\n"+"2.Float Number"+"\n"+"3.String");
+		Scanner sc=new Scanner(System.in);
+		int option=sc.nextInt();
 		MaximumNumber maximumNumber=new MaximumNumber();
-		maximumNumber.testMaximumIntegerNumber();
-		System.out.println("maximum integer number:"+MAX);
-		maximumNumber.testMaximumFloatNumber();
-		System.out.println("maximum float number:"+MAX1);
+		switch(option) {
+		case 1:
+			maximumNumber.testMaximumIntegerNumber();
+			System.out.println("maximum integer number:"+MAX);
+			break;
+		case 2:
+			maximumNumber.testMaximumFloatNumber();
+			System.out.println("maximum float number:"+MAX1);
+			break;
+		case 3:
+			maximumNumber.testMaximumString();
+			System.out.println("maximum string:"+MAX2);
+			break;
+		default:
+				System.out.println("Invalid option");
+		}	
 	}
 	
 	public int testMaximumIntegerNumber() {
@@ -51,5 +67,24 @@ public class MaximumNumber<E extends Comparable<E>> {
 			MAX1=number.getNumber3();
 		}
 		return MAX1;
+	}
+	
+	public String testMaximumString() {
+		Scanner sc=new Scanner(System.in);
+		Test<String> number=new Test<String>();
+		System.out.println("Enter a first String:");
+		number.setNumber1((sc.next()));
+		System.out.println("Enter a second String:");									
+		number.setNumber2((sc.next()));
+		System.out.println("Enter a third String:");
+		number.setNumber3((sc.next()));								
+		MAX2=number.getNumber1();
+		if(number.getNumber2().compareTo(MAX2)>0) {
+			MAX2=number.getNumber2();
+		}
+		if(number.getNumber3().compareTo(MAX2)>0) {
+			MAX2=number.getNumber3();
+		}
+		return MAX2;
 	}
 }
